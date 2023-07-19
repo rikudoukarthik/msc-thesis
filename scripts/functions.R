2d52c1208053ee803a0e526cb5392cd4845ba48a
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2023-07-19 18:53:13 +0530

    resolved bootMer issue with glmmTMB (see https://stackoverflow.com/a/76694251/13000254)

--- scripts/functions.R
+++ scripts/functions.R
@@ -5,7 +5,9 @@
 
 boot_conf_GLMM = function(model, 
                           new_data, # separately specify dataframe with vars for model
-                          new_data_string, # string for clusterExport()
+                          # string for clusterExport()
+                          new_data_string, 
+                          model_data_string,
                           re_form = NA,
                           nsim = 1000,
                           pred_type = "link")
@@ -16,17 +18,18 @@ boot_conf_GLMM = function(model,
   require(lme4)
   require(VGAM)
   require(parallel) # to parallelise bootstrap step
+  require(glmmTMB)
   
-  pred_fun <- function(model) {
-    predict(model, newdata = new_data, type = pred_type, re.form = re_form, 
+  pred_fun <- function(some_model) {
+    predict(some_model, newdata = new_data, type = pred_type, re.form = re_form, 
             allow.new.levels = TRUE)
     # not specifying type = "response" because will later transform prediction along with SE
   }
   
   par_cores <- max(1, floor(detectCores()/2))
   par_cluster <- makeCluster(rep("localhost", par_cores), outfile = "log.txt")
-  clusterEvalQ(par_cluster, library(lme4))
-  clusterExport(par_cluster, varlist = new_data_string)
+  clusterEvalQ(par_cluster, library("glmmTMB"))
+  clusterExport(par_cluster, varlist = c(new_data_string, model_data_string))
   
   print(glue("Using {par_cores} cores."))
   
@@ -38,6 +41,8 @@ boot_conf_GLMM = function(model,
   stopCluster(par_cluster)
   
   return(pred_bootMer$t)
+  
+  # attr(pred_bootMer, "boot.fail.msgs") # from https://stackoverflow.com/a/76694251/13000254
   
 }
 
